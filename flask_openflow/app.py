4278b86f64672b2d6e856b4c85b3a11b883e53a6
ddeniro-hash <ddeniro-hash>
2024-10-14 10:26:36 -0600

    initial commit

--- flask_openflow/app.py
+++ flask_openflow/app.py
@@ -171,6 +171,7 @@ def gitpush():
     subprocess.run(["git", "init"], check=True)
     subprocess.run(["git", "add", "."], check=True)
     subprocess.run(["git", "commit", "-m", "initial commit"], check=True)
+    subprocess.run(["git", "remote", "remove", "origin"], check=True, stderr=subprocess.DEVNULL)
     remote_url = f"https://{token}@github.com/ddeniro-hash/SDN-Midterm.git"
     subprocess.run(["git", "remote", "add", "origin", remote_url], check=True)
     subprocess.run(["git", "push", "-u", "origin", "master"], check=True)
